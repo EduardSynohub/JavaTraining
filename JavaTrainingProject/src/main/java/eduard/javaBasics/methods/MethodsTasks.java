53d20ba0c1c358612dfe29b4242617ea111255e1
eduard <eduard.synohub@gmail.com>
2025-08-31 16:08:16 +0200

    basic tasks in Java (methods)

--- JavaTrainingProject/src/main/java/eduard/javaBasics/methods/MethodsTasks.java
+++ JavaTrainingProject/src/main/java/eduard/javaBasics/methods/MethodsTasks.java
@@ -43,12 +43,30 @@ public class MethodsTasks {
         int num3 = 58;
         System.out.println(maxOfThree(num1, num2, num3));
 
-//    task 9
+//        task 9
         int goalsTeamAonFieldA = 0;
         int goalsTeamAonFieldB = 3;
         int goalsTeamBonFieldA = 2;
         int goalsTeamBonFieldB = 1;
-        System.out.println(footballWin(goalsTeamAonFieldA, goalsTeamBonFieldA, goalsTeamAonFieldB, goalsTeamBonFieldB));;
+        System.out.println(footballWin(goalsTeamAonFieldA, goalsTeamBonFieldA, goalsTeamAonFieldB, goalsTeamBonFieldB));
+
+//        task 10
+        double dogAge1 = 1.5;
+        double dogAge2 = 5;
+        System.out.println(dogAge(dogAge1));
+        System.out.println(dogAge(dogAge2));
+
+//        task 11
+        int num = 5;
+        System.out.println(factorial(num));
+        System.out.println(factorial(3));
+
+//        task 12
+        int a = 15;
+        int b = 5;
+        int c = 4;
+        System.out.println(divisibleBy(a, b));
+        System.out.println(divisibleBy(a, c));
     }
 
 //    task 1
@@ -115,6 +133,33 @@ public class MethodsTasks {
             return "Wygrał zespół A.";
         } else {
             return "Wygrał zespół B.";
+        }
+    }
+
+//    task 10
+    public static double dogAge(double dogAge) {
+        if (dogAge > 0 && dogAge <=2) {
+            return dogAge * 10.5;
+        } else {
+            return (2 * 10.5) + ((dogAge - 2) * 4);
+        }
+    }
+
+//    task 11
+    public static int factorial(int number) {
+        int result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+//    task 12
+    public static boolean divisibleBy(int a, int b) {
+        if (a % b == 0) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
